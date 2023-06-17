90e2a1c4c09f476280cd8fc4fcf455e8a1fac1dc
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2023-06-17 22:21:49 +0300

    filter factory empty class created

---
+++ factory/FilterFactory.hpp
@@ -0,0 +1,25 @@
+/* -*-c++-*- 
+ *
+ *    file: FilterFactory.hpp
+ *  author: ShpakovDmitry
+ *    date: 2023-6-17
+ * licence: CC0
+ * summary: Factory to create instances of various filters.
+ *
+ */
+
+#ifndef _FILTER_FACTORY_HPP
+#define _FILTER_FACTORY_HPP
+
+#include "../filters.hpp"
+
+class FilterFactory {
+    public:
+
+    private:
+
+};
+
+
+#endif//_FILTER_FACTORY_HPP
+
