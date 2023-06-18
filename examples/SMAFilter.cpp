84d76e20670aec2b42f895441409a943e9069e1e
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2023-06-18 17:41:41 +0300

    added usage example of SMA filter

---
+++ examples/SMAFilter.cpp
@@ -0,0 +1,43 @@
+/* -*-c++-*- 
+ *
+ *    file: SMAFilter.cpp
+ *  author: ShpakovDmitry
+ *    date: 2023-6-18
+ * licence: CC0
+ * summary: Usage example of Simple Moving Average filter.
+ *
+ */
+
+#include "../filters"
+#include <iostream>
+
+using namespace std;
+using namespace Filters;
+
+constexpr unsigned SAMPLES_TO_FILTER = 10;
+constexpr int SAMPLES_TO_READ_NUM = 100;
+extern float getSample();
+extern vector<float> getSamples(int num);
+
+int main(int argc, char *argv[]) {
+    
+    unique_ptr<Filter> SMAFilter = FilterFactory::createSimpleMovingAverageFilter(SAMPLES_TO_FILTER);
+
+    // filter sample by sample
+    for (int i = 0; i < SAMPLES_TO_READ_NUM; ++i) {
+        float input = getSample();
+        float output = SMAFilter->filterSample(input);
+        cout << input << " " << output << endl;
+    }
+
+    SMAFilter->reset();
+
+    // filter samples 
+    vector<float> samples = getSamples(SAMPLES_TO_READ_NUM);
+    vector<float> filteredSamples = SMAFilter->filterSamples(samples);
+    for (int i = 0; i < samples.size(); ++i) {
+        cout << samples[i] << " " << filteredSamples[i] << endl;
+    }
+
+    return 0;
+}
