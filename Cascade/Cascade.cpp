7bcde0833dda7a64af5b7b5a52d420884bb32565
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2023-06-18 23:19:09 +0300

    added Cascade implementation

---
+++ Cascade/Cascade.cpp
@@ -0,0 +1,47 @@
+/* -*-c++-*- 
+ *
+ *    file: Cascade.cpp
+ *  author: ShpakovDmitry
+ *    date: 2023-6-18
+ * licence: CC0
+ * summary: Class for Filter cascading implementation.
+ *
+ */
+
+#include "Cascade"
+
+namespace Filters {
+
+    Cascade::Cascade() {}
+
+    void Cascade::reset() {
+        for (Filter *filter : m_filters) {
+            filter->reset();
+        }
+    }
+
+    float Cascade::filterSample(float sample) {
+        float filteredSample = sample;
+        for (Filter *filter : m_filters) {
+            filteredSample = filter->filterSample(filteredSample);
+        }
+        return filteredSample;
+    }
+
+    std::vector<float> Cascade::filterSamples(const std::vector<float> &samples) {
+        std::vector<float> filteredSamples;
+        filteredSamples.reserve(samples.size());
+        
+        for (const auto &sample : samples) {
+            float filteredSample = filterSample(sample);
+            filteredSamples.push_back(filteredSample);
+        }
+        return filteredSamples;
+    }
+
+    void Cascade::addFilter(Filter *filter) {
+        m_filters.push_back(filter);
+    }
+
+}
+
